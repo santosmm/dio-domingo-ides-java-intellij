72eb644d45349387693d9243f51fdc1a7ef4cfc8
santosmm <matheusmottas@gmail.com>
2021-10-17 17:00:16 -0300

    Testando integração Git com Intellij

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,5 +4,6 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         System.out.println("Hello Word!");
         System.out.println("Hello Word!");
+        System.out.println("Hello Word!");
     }
 }
